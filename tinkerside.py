0ad0a29ba6229147e2cf64dcb4e162ee3b72a5ea
Oğuzhan Turhan <oguzhant465@gmail.com>
2023-03-16 14:18:59 +0300

    Update tinkerside.py

--- tinkerside.py
+++ tinkerside.py
@@ -1,7 +1,7 @@
 import serial
-
 ser = serial.Serial('/dev/ttymxc0', 115200) # replace ttyS1 with the appropriate serial port
-ser.write(b'Hello Raspberry Pi!\n')
-response = ser.readline()
-print(response)
-ser.close()
+while True:
+
+  ser.write(b'Hello Raspberry Pi!\n')
+  response = ser.readline()
+  
