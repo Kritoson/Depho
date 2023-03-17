88dc91f01dcdf40436985ee1312b0d491d92f876
Mustafa GÜLSOY <mustafa30045@gmail.com>
2023-03-17 03:00:03 +0300

    Added comparition function and port protocols

--- multicam.py
+++ multicam.py
@@ -6,6 +6,21 @@ cap = cv2.VideoCapture('rtsp://admin:123456@192.168.1.237/H264?ch=1&subtype=0')
 ser = serial.Serial('/dev/ttymxc0', 115200) # replace ttyS1 with the appropriate serial port
 message = ''
 
+
+def send_data(cor):
+    package = b''
+
+    for i in cor:
+        package += struct.pack('!i', i)
+        print(package)
+
+    # Sent string value,but if tests shows us it is wrong turn it on btye
+    ser.write(package.encode('utf-8'))
+
+    # Do nothing for 500 milliseconds (0.5 seconds)
+    time.sleep(0.5)
+
+    
 while True:
     _, frame = cap.read()
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
